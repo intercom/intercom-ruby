ef665da1af7f8fd60c085ebc9716fe7add090255
James Osler <j.osler@gmail.com>
2014-09-04 15:41:26 -0400

    dont try to update empty emails

--- lib/ext/sliceable_hash.rb
+++ lib/ext/sliceable_hash.rb
@@ -7,6 +7,10 @@ class SliceableHash
   # Return a hash that includes only the given keys.
   def slice(*keys)
     keys.map! { |key| @hash.convert_key(key) } if @hash.respond_to?(:convert_key, true)
-    keys.each_with_object(@hash.class.new) { |k, hash| hash[k] = @hash[k] if @hash.has_key?(k) }
+    keys.each_with_object(@hash.class.new) { |k, hash| hash[k] = @hash[k] if @hash.has_key?(k) && if_string_not_empty(@hash[k]) }
+  end
+  
+  def if_string_not_empty(val)
+    val.kind_of?(String) ? !val.empty? : true
   end
 end
