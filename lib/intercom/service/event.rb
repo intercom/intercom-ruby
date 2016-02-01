84c0b9a5cbd88988e5d30c7f9a3e3d7bab42515f
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-02-01 17:52:02 +0100

    Experimental event search

--- lib/intercom/service/event.rb
+++ lib/intercom/service/event.rb
@@ -1,10 +1,14 @@
 require 'intercom/service/base_service'
+require 'intercom/api_operations/find'
+require 'intercom/api_operations/find_all'
 require 'intercom/api_operations/save'
 require 'intercom/api_operations/bulk/submit'
 
 module Intercom
   module Service
     class Event < BaseService
+      include ApiOperations::Find
+      include ApiOperations::FindAll
       include ApiOperations::Save
       include ApiOperations::Bulk::Submit
 
