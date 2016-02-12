a2370093d1d0c4a0a401946fc65f88be42bbe42a
Corey Leveen <mntj11@gmail.com>
2016-02-11 23:35:41 -0500

    Error documentation cleanup

--- lib/intercom/errors.rb
+++ lib/intercom/errors.rb
@@ -14,7 +14,7 @@ module Intercom
   # Check that you have set <b>Intercom.app_id=</b> and <b>Intercom.app_api_key=</b> correctly.
   class AuthenticationError < IntercomError; end
 
-  # Raised when something does wrong on within the Intercom API service.
+  # Raised when something goes wrong on within the Intercom API service.
   class ServerError < IntercomError; end
 
   # Raised when we have bad gateway errors.
@@ -29,21 +29,21 @@ module Intercom
   # Raised when requesting resources on behalf of a user that doesn't exist in your application on Intercom.
   class ResourceNotFound < IntercomError; end
 
-  # Raised when the request has a bad syntax
+  # Raised when the request has bad syntax
   class BadRequestError < IntercomError; end
 
-  # Raised when you have exceed the API rate limit
+  # Raised when you have exceeded the API rate limit
   class RateLimitExceeded < IntercomError; end
 
   # Raised when the request throws an error not accounted for
   class UnexpectedError < IntercomError; end
-  
+
   # Raised when multiple users match the query (typically duplicate email addresses)
   class MultipleMatchingUsersError < IntercomError; end
 
   # Raised when you try to call a non-setter method that does not exist on an object
   class Intercom::AttributeNotSetError < IntercomError ; end
-  
+
   # Raised when unexpected nil returned from server
   class Intercom::HttpError < IntercomError ; end
 
@@ -51,7 +51,7 @@ module Intercom
   # Non-public errors (internal to the gem)
   #
 
-  # Base class exception from which all public Intercom exceptions will be derived
+  # Base class exception from which all private Intercom exceptions will be derived
   class IntercomInternalError < StandardError; end
 
   # Raised when we attempt to handle a method missing but are unsuccessful
