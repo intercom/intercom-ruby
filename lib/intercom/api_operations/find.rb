da51e6501c089d88704b9b86aacbfeb4d93dc9e1
Bob Long <robertjflong@gmail.com>
2016-06-23 18:05:22 +0100

    Pass extra params with #find

--- lib/intercom/api_operations/find.rb
+++ lib/intercom/api_operations/find.rb
@@ -5,7 +5,7 @@ module Intercom
         raise BadRequestError, "#{self}#find takes a hash as its parameter but you supplied #{params.inspect}" unless params.is_a? Hash
         collection_name = Utils.resource_class_to_collection_name(collection_class)
         if params[:id]
-          response = @client.get("/#{collection_name}/#{params[:id]}", {})
+          response = @client.get("/#{collection_name}/#{params[:id]}", params)
         else
           response = @client.get("/#{collection_name}", params)
         end
