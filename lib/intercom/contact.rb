f945a6cc1380acc1ae0b08d5bd046ecc1bc3c015
Marin Martinic <marin@intercom.io>
2017-11-27 10:19:03 +0000

    add data attributes accessors

--- lib/intercom/contact.rb
+++ lib/intercom/contact.rb
@@ -1,8 +1,10 @@
 require 'intercom/traits/api_resource'
+require 'intercom/traits/data_attribute_accessors'
 
 module Intercom
   class Contact
     include Traits::ApiResource
+    include Traits::DataAttributeAccessors
 
     def identity_vars ; [:email, :user_id] ; end
     def flat_store_attributes ; [:custom_attributes] ; end
---
+++ lib/intercom/data_attribute.rb
@@ -0,0 +1,7 @@
+require 'intercom/traits/api_resource'
+
+module Intercom
+  class DataAttribute
+    include Traits::ApiResource
+  end
+end
